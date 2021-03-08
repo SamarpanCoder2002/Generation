d687681d4061749545916fc44ae40b352121d4e4
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2021-03-09 00:52:41 +0530

    Search Icon Roundable Two Option UI Added

--- lib/MainWindow.dart
+++ lib/MainWindow.dart
@@ -2,22 +2,14 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:slide_drawer/slide_drawer.dart';
+import 'package:fab_circular_menu/fab_circular_menu.dart';
 
 class MainScreen extends StatefulWidget {
   @override
   _MainScreenState createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen>
-    with SingleTickerProviderStateMixin {
-  TabController _tabController;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    _tabController = TabController(length: 3, vsync: this);
-  }
-
+class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -99,6 +91,43 @@ class _MainScreenState extends State<MainScreen>
               ],
             ),
           ),
+          floatingActionButton: FabCircularMenu(
+            fabColor: Color.fromRGBO(0, 230, 150, 1),
+            fabOpenIcon: Icon(
+              Icons.search_rounded,
+              color: Colors.white,
+              size: 30.0,
+            ),
+            fabOpenColor: Color.fromRGBO(192, 192, 240, 1),
+            fabIconBorder: CircleBorder(
+              side: BorderSide(width: 0.2, color: Colors.black45),
+            ),
+            fabCloseIcon: Icon(Icons.close_outlined, color: Colors.red, size:30.0,),
+            ringDiameter: 325.0,
+            ringWidth: 80.0,
+            ringColor: Color.fromRGBO(64, 196, 255, 0.6),
+            animationCurve: Curves.easeInOutSine,
+            children: [
+              IconButton(
+                  icon: Icon(
+                    Icons.person_search_rounded,
+                    size: 50.0,
+                    color: Color.fromRGBO(16, 24, 255, 1),
+                  ),
+                  onPressed: () {
+                    print('Home');
+                  }),
+              IconButton(
+                  icon: Icon(
+                    Icons.settings_voice_sharp,
+                    size: 50.0,
+                    color:Color.fromRGBO(16, 24, 255, 1),
+                  ),
+                  onPressed: () {
+                    print('Home');
+                  }),
+            ],
+          ),
           body: TabBarView(
             children: [
               chatScreen(context),
@@ -141,12 +170,11 @@ class _MainScreenState extends State<MainScreen>
   Widget statusList(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
-        primary: Colors.white24,
-        elevation: 0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(80),
-        )
-      ),
+          primary: Colors.white24,
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(80),
+          )),
       onPressed: () {
         print("Status Image Clicked");
       },
@@ -191,98 +219,97 @@ class _MainScreenState extends State<MainScreen>
 
   Widget chatTile(BuildContext context) {
     return Card(
-
         child: Container(
-          padding: EdgeInsets.only(left: 1.0, right: 1.0),
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.white,
-              onPrimary: Colors.lightBlueAccent,
-            ),
-            onPressed: () {
-              print("Pressed");
-            },
-            child: Row(
-              children: [
-                Container(
-                  //color: Colors.green,
-                  padding: EdgeInsets.only(
-                    top: 5.0,
-                    bottom: 5.0,
-                  ),
-                  child: GestureDetector(
-                    child: CircleAvatar(
-                      radius: 30.0,
-                      backgroundImage: ExactAssetImage("images/sam.jpg"),
-                    ),
-                    onTap: () {
-                      print("Pic Pressed");
-                    },
-                  ),
+      padding: EdgeInsets.only(left: 1.0, right: 1.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.white,
+          onPrimary: Colors.lightBlueAccent,
+        ),
+        onPressed: () {
+          print("Pressed");
+        },
+        child: Row(
+          children: [
+            Container(
+              //color: Colors.green,
+              padding: EdgeInsets.only(
+                top: 5.0,
+                bottom: 5.0,
+              ),
+              child: GestureDetector(
+                child: CircleAvatar(
+                  radius: 30.0,
+                  backgroundImage: ExactAssetImage("images/sam.jpg"),
                 ),
-                Container(
-                  //color: Colors.redAccent,
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width / 2 + 20,
-                  padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Samarpan Dasgupta",
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Colors.black,
-                        ),
+                onTap: () {
+                  print("Pic Pressed");
+                },
+              ),
+            ),
+            Container(
+              //color: Colors.redAccent,
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width / 2 + 20,
+              padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+              child: Column(
+                children: [
+                  Text(
+                    "Samarpan Dasgupta",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12.0,
+                  ),
+                  Container(
+                    //color: Colors.blueGrey,
+                    //padding: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      "New Message Alert",
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.black45,
                       ),
-                      SizedBox(
-                        height: 12.0,
-                      ),
-                      Container(
-                        //color: Colors.blueGrey,
-                        //padding: EdgeInsets.only(left: 10.0),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                alignment: Alignment.centerRight,
+                //color: Colors.deepPurpleAccent,
+                padding: EdgeInsets.only(
+                  right: 20.0,
+                  top: 2.0,
+                  bottom: 2.0,
+                ),
+                child: Column(
+                  children: [
+                    Container(
                         child: Text(
-                          "New Message Alert",
-                          style: TextStyle(
-                            fontSize: 15.0,
-                            color: Colors.black45,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    alignment: Alignment.centerRight,
-                    //color: Colors.deepPurpleAccent,
-                    padding: EdgeInsets.only(
-                      right: 20.0,
-                      top: 2.0,
-                      bottom: 2.0,
+                      "12:00",
+                      style: TextStyle(fontSize: 12.0, color: Colors.blue),
+                    )),
+                    SizedBox(
+                      height: 10.0,
                     ),
-                    child: Column(
-                      children: [
-                        Container(
-                            child: Text(
-                          "12:00",
-                          style: TextStyle(fontSize: 12.0, color: Colors.blue),
-                        )),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Container(
-                          child: Icon(
-                            Icons.surround_sound,
-                            color: Colors.green,
-                          ),
-                        ),
-                      ],
+                    Container(
+                      child: Icon(
+                        Icons.surround_sound,
+                        color: Colors.green,
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ));
+          ],
+        ),
+      ),
+    ));
   }
 }
