de9b16d23c156aca53b28b3229567e55ac840138
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2021-04-04 15:22:07 +0530

    Search Functionality Modified

--- lib/FrontEnd/Services/search_screen.dart
+++ lib/FrontEnd/Services/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Search extends StatefulWidget {
@@ -7,6 +8,9 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  Icon _iconSample = Icon(Icons.filter_list_rounded);
+  String searchArgument;
+
   TextEditingController searchUser = TextEditingController();
   QuerySnapshot searchResultSnapshot;
 
@@ -20,7 +24,11 @@ class _SearchState extends State<Search> {
       });
       await FirebaseFirestore.instance
           .collection("generation_users")
-          .where("user_name", isEqualTo: searchUser.text)
+          .where(
+            searchArgument,
+            isGreaterThanOrEqualTo: searchUser.text
+                .toUpperCase(), // We know that, for both ASCII or Unicode, small letters came after capital letters....//So, search query always find the relevant result according to search
+          )
           .get()
           .catchError((e) {
         print(e.toString());
@@ -41,9 +49,11 @@ class _SearchState extends State<Search> {
             shrinkWrap: true,
             itemCount: searchResultSnapshot.docs.length,
             itemBuilder: (context, index) {
+              print(searchResultSnapshot.docs[index]);
               return userTile(
-                searchResultSnapshot.docs[index]["user_name"],
-                searchResultSnapshot.docs[index].id,
+                searchResultSnapshot.docs[index][searchArgument],
+                searchResultSnapshot.docs[index]
+                    [searchArgument == "about" ? "nick_name" : "about"],
               );
             })
         : Container(
@@ -56,27 +66,51 @@ class _SearchState extends State<Search> {
           );
   }
 
-  Widget userTile(String userName, String userEmail) {
+  Widget userTile(String userName, String userAbout) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      width: double.maxFinite,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                userName,
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              Text(
-                userEmail,
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              )
-            ],
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  userName,
+                  style: TextStyle(
+                      color: Colors.orange,
+                      fontSize: searchArgument != "about" ? 20 : 15),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  userAbout,
+                  style: TextStyle(color: Colors.lightBlue, fontSize: 14),
+                ),
+              ],
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.person_add_alt_1_outlined,
+              size: 30.0,
+              color: Colors.green,
+            ),
+            onPressed: () {},
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    searchArgument = "nick_name";
   }
 
   @override
@@ -87,14 +121,15 @@ class _SearchState extends State<Search> {
         //color: Colors.black87,
         width: double.maxFinite,
         height: MediaQuery.of(context).size.height,
-        child: Column(
+        child: ListView(
+          shrinkWrap: true,
           children: [
             Container(
               padding: EdgeInsets.only(
                 left: 24,
                 right: 24,
-                top: 25,
-                bottom: 5,
+                top: 5,
+                bottom: 10,
               ),
               margin: EdgeInsets.only(bottom: 20.0),
               decoration: BoxDecoration(
@@ -118,6 +153,10 @@ class _SearchState extends State<Search> {
                       decoration: InputDecoration(
                         border: InputBorder.none,
                         labelText: "Enter Username",
+                        suffixIcon: IconButton(
+                          icon: _iconSample,
+                          onPressed: filterOptions,
+                        ),
                         labelStyle: TextStyle(
                           color: Colors.green,
                           fontSize: 16,
@@ -146,5 +185,68 @@ class _SearchState extends State<Search> {
         ),
       ),
     );
+  }
+
+  void filterOptions() async {
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              backgroundColor: Color.fromRGBO(34, 48, 60, 1),
+              shape: CircleBorder(),
+              title: Center(
+                child: Text(
+                  "Filter",
+                  style: TextStyle(
+                      color: Colors.lightGreen,
+                      letterSpacing: 1.0,
+                      fontSize: 23.0,
+                      fontFamily: 'Lora'),
+                ),
+              ),
+              content: SizedBox(
+                width: double.maxFinite,
+                child: ListView(
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    TextButton(
+                      child: Text(
+                        "Nick Name",
+                        style: TextStyle(color: Colors.orange),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          searchArgument = "nick_name";
+                          Navigator.pop(context);
+                        });
+                      },
+                    ),
+                    TextButton(
+                      child: Text(
+                        "User Name",
+                        style: TextStyle(color: Colors.orange),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          searchArgument = "user_name";
+                          Navigator.pop(context);
+                        });
+                      },
+                    ),
+                    TextButton(
+                      child: Text(
+                        "About",
+                        style: TextStyle(color: Colors.orange),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          searchArgument = "about";
+                          Navigator.pop(context);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ));
   }
 }
