e72bdb6973b91f09f2d1c69274ee91ca98bff700
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2022-02-27 13:22:29 +0530

    Changes

--- README.md
+++ README.md
@@ -1,132 +1,16 @@
-![](https://img.shields.io/badge/Programming_Language-Dart-blue.svg)
-![](https://img.shields.io/badge/Software_Development_Kit(SDK)-Flutter-gold.svg)
-![](https://img.shields.io/badge/Platform-Android_Studio-green.svg)
-![](https://img.shields.io/badge/App-Android-yellow.svg)
-![](https://img.shields.io/badge/Flutter_Version-2.5.0-brown.svg)
-![](https://img.shields.io/badge/Dart_Version-2.14.0-orange.svg)
-![](https://img.shields.io/badge/Status-Stable-blue.svg)
-![](https://img.shields.io/badge/Null_Safety-Migrated-green.svg)
+# generation
 
+A new Flutter project.
 
-# <p align="center">💙 Generation 💙</p>
+## Getting Started
 
-#### <p align="center"> ✨ A Private, Secure, End-to-End Encrypted Messaging app made in Flutter that helps you to connect with your connections without any Ads, promotion. No other third-party person, organization, or even Generation Team can't read your messages. ✨ </p>
+This project is a starting point for a Flutter application.
 
-#### <p align="center"> 💖 Your Privacy is the biggest priority. 💖</p>
+A few resources to get you started if this is your first Flutter project:
 
-### <p align="center"> Generation App Sample Looks </p>
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
 
-<p align="center">
-
-<img src="./readme_documents/1.png" width=200 alt="1st sample"/>
-<img src="./readme_documents/2.png" width=200 alt="2nd sample"/>
-<img src="./readme_documents/3.png" width=200 alt="3rd sample"/>
-<img src="./readme_documents/4.png" width=200 alt="4th sample"/>
-<img src="./readme_documents/5.png" width=200 alt="5th sample"/>
-<img src="./readme_documents/6.png" width=200 alt="6th sample"/>
-<img src="./readme_documents/7.png" width=200 alt="7th sample"/>
-<img src="./readme_documents/8.png" width=200 alt="8th sample"/>
-
-</p>
-
-<p align="center">
-  
-#### ✨ Please Read [CONTRIBUTION.md](https://github.com/SamarpanCoder2002/Generation/blob/main/CONTRIBUTING.md) file before contributing in that repo.
-  
-</p>
-
-### 🎯 Authentication Types:-
-| Email and Password Authentication 	| Google Authentication 	|
-|:-:	|-	|
-
-<br/>
-
-### 🎯 Special Features:-
-#### 1. Chat Message Conversation
-
-|               	| Chat Types|            	|            	|
-|---------------	|--------------------------	|------------	|------------	|
-|   Text Chat   	| Voice Chat               	| Image Chat 	| Video Chat 	|
-| Document Chat 	| Location Chat            	| Audio Chat 	|            	|
-
-
-#### 2. Activity Section
-|               	| Activity Types 	|                  	|
-|---------------	|----------------	|------------------	|
-| Text Activity 	| Image Activity 	| Polling Activity 	|
-
-
-#### 3. Connection Request Accept And New Connection Request Feature
-#### 4. Multiple Connection Message Send Simultaneously
-
-|  	|      Types   	|  	|
-|-	|-	|-	|
-| Text Chat 	| Image Chat 	| Voice Chat 	|
-| Audio Chat 	| Location Chat 	| Document Chat 	|
-| Video Chat 	|  	|  	|
-
-#### 5. Media Visibility Section
-|  	|  Media Visibility  	|  	|  	|
-|-	|-	|-	|-	|
-| Image 	|        Video 	|    Document 	|        Audio        	|
-#### 6. Notification Settings Available for Global and Manual Chat
-
-|  	| Notification Types 	|  	|  	|
-|-	|-	|-	|-	|
-| Background Notification Annotation 	| Online Notification 	| Remove Birth Notification 	| Remove Anonymous Notification 	|
-
-#### 7. Chat Wallpaper
-
-| Global Chat Wallpaper 	| Manual Chat Wallpaper 	|
-|:-:	|-	|
-
-#### 8. Support Section
-
-| Report a Problem 	| Request a Feature 	| Send Feedback 	| Donate(Payment Gateway Integrated) 	|
-|:-:	|-	|-	|-	|
-
-#### 9.  Internal Search Section
-#### 10. Audio Calling and Calling History Section(Beta)
-#### 11. Profile Image Feature
-#### 12. Chat Message Forward Feature
-#### 13. Share Chat Contents via multiple apps
-#### 14. Delete for Me for any chat messages
-
-</br>
-
-### 👀 Main APIs Used
-| Firebase API 	| Google Map API 	|
-|:-:	|-	|  
-
-</br>
-
-### 🎯 Plugins Used
-#### 👉 Please see in [`pubspec.yaml`](https://github.com/SamarpanCoder2002/Generation/blob/main/pubspec.yaml) file
-
-</br>
-
-### 🎯 Important Points Needs to Know
-
-#### 💥 Some Feature of this app written in Native part(Kotlin)
-#### 💥 Screenshot and Screen Recording is restricted for this app due to Security
-#### 💥 Except Audio Calling, all Messages and Activity are End-to-End-Encrypted
-#### 💥 Activity(Status) will deleted automatically after 24 hrs after it's launch
-#### 💥 API keys along with google-services.json file and Secure Keys removed for unwanted or unauthorized uses
-
-</br>
-
-### 🙋 For Any query related to this project(Generation), Please Raise an issue in that Repo.
-
-### 💥 This Project is Participating Under `Hacktoberfest 2021`
-
-***
-
-<h3 align="center">Show 💘 by Starring This Repo</h3>
-
-- #### [Click Here to See the Project Video](https://youtu.be/QEDhmxzEqUA)
-
-- #### [LinkedIN Video](https://www.linkedin.com/posts/samarpan-dasgupta-4aa1061b0_connections-introducing-project-activity-6811146891833704448-FyrC)
-
-- #### [APK File Download Link](https://drive.google.com/file/d/1KbyV40g-oUjdEwLzmEfmm9sjnwBu5D2b/view?usp=sharing)
-
-<h3 align="center"><b>🧡 Thank You For Visiting 🙏, Have a Nice Day 🧡</b></h3>
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,177 +1,115 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:generation/BackendAndDatabaseManager/native_internal_call/native_call.dart';
 
-import 'package:generation/FrontEnd/Introduction_Screen/intro_screen.dart';
-import 'package:generation/FrontEnd/MainScreen/main_window.dart';
-import 'package:generation/BackendAndDatabaseManager/global_controller/different_types.dart';
-import 'package:generation/BackendAndDatabaseManager/sqlite_services/local_storage_controller.dart';
-import 'package:generation/BackendAndDatabaseManager/firebase_services/google_auth.dart';
-import 'package:generation/BackendAndDatabaseManager/general_services/notification_configuration.dart';
-
-final GlobalKey<NavigatorState>? navigatorKey = GlobalKey<NavigatorState>();
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  final LocalStorageHelper _localStorageHelper = LocalStorageHelper();
-
-  /// Initialize Notification Settings
-  await notificationInitialize();
-
-  /// For Background Message Handling
-  FirebaseMessaging.onBackgroundMessage(backgroundMsgAction);
-
-  /// For Foreground Message Handling
-  FirebaseMessaging.onMessage.listen((messageEvent) async {
-    print(
-        'Message Data is: ${messageEvent.notification!.title}      ${messageEvent.notification!.body}');
-
-    final bool _fgNotifyStatus =
-        await _localStorageHelper.extractDataForNotificationConfigTable(
-            nConfigTypes: NConfigTypes.FGNotification);
-
-    print('Foreground Notification Status: $_fgNotifyStatus');
-
-    if (_fgNotifyStatus) {
-      if (messageEvent.notification!.title!.toString().contains('Connection Request') ||
-          messageEvent.notification!.title!.toString().contains('New Connection')) {
-        _receiveAndShowNotificationInitialization(
-          title: messageEvent.notification!.title.toString(),
-          body: messageEvent.notification!.body.toString(),
-        );
-      } else {
-        final String _userName = messageEvent.notification!.title!.toString().split(' ')[0];
-
-        print('Foreground Notification Comer User Name: $_userName');
-
-        final bool _fgStatus =
-            await _localStorageHelper.extractImportantTableData(
-                extraImportant: ExtraImportant.FGNStatus, userName: _userName);
-
-        if (_fgStatus)
-          _receiveAndShowNotificationInitialization(
-            title: messageEvent.notification!.title.toString(),
-            body: messageEvent.notification!.body.toString(),
-          );
-        else
-          print('$_userName Foreground notification off');
-      }
-    } else
-      print('Global Notification Permission Denied');
-  }, onDone: () => print('Done'), onError: (e) => print('Error: $e'));
-
-  // /// Change Navigation Bar Color
-  // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-  //   systemNavigationBarColor: Colors.black54,
-  // ));
-
-  runApp(MaterialApp(
-    title: 'Generation',
-    debugShowCheckedModeBanner: false,
-    themeMode: ThemeMode.dark,
-    home: await differentContext(),
-  ));
+void main() {
+  runApp(const MyApp());
 }
 
-/// Receive And Show Notification Customization
-void _receiveAndShowNotificationInitialization(
-    {required String title, required String body}) async {
-  final ForeGroundNotificationReceiveAndShow
-      _foregroundNotificationReceiveAndShow =
-      ForeGroundNotificationReceiveAndShow();
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  /// Show Notification When App is On
-  print('Here');
-
-  await _foregroundNotificationReceiveAndShow.showNotification(
-    title: title,
-    body: body,
-  );
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 
-Future<void> notificationInitialize() async {
-  /// Important to subscribe a topic to send and receive message using FCM via http
-  await FirebaseMessaging.instance.subscribeToTopic('Generation');
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  /// Foreground Notification Options Enabled
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-/// Background Message Show for Debugging
-Future<void> backgroundMsgAction(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  final NativeCallback nativeCallback = NativeCallback();
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  print(
-      'Background Message Data: ${message.notification!.body}   ${message.notification!.title}');
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
 
-  final bool _bgNotifyStatus = await LocalStorageHelper()
-      .extractDataForNotificationConfigTable(
-          nConfigTypes: NConfigTypes.BgNotification);
-
-  print('Background Notification Status: $_bgNotifyStatus');
-
-  await nativeCallback.callForCancelNotifications();
-
-  if (_bgNotifyStatus) {
-    if (message.notification!.title!.contains('Connection Request') ||
-        message.notification!.title!.contains('New Connection')) {
-      _receiveAndShowNotificationInitialization(
-        title: message.notification!.title.toString(),
-        body: message.notification!.body.toString(),
-      );
-    } else {
-      final String _userName = message.notification!.title!.split(' ')[0];
-
-      print('Background Notification Comer User Name: $_userName');
-
-      final bool _bgStatus = await LocalStorageHelper()
-          .extractImportantTableData(
-              extraImportant: ExtraImportant.BGNStatus, userName: _userName);
-
-      if (_bgStatus)
-        _receiveAndShowNotificationInitialization(
-          title: message.notification!.title.toString(),
-          body: message.notification!.body.toString(),
-        );
-      else
-        print('$_userName Background notification off');
-    }
-  } else
-    print('Background Global Notification Permission Denied');
-}
-
-/// Decide to Switch to widget based of current Scenario
-Future<Widget> differentContext() async {
-  if (FirebaseAuth.instance.currentUser == null) return IntroductionScreen();
-
-  try {
-    final DocumentSnapshot responseData = await FirebaseFirestore.instance
-        .doc("generation_users/${FirebaseAuth.instance.currentUser!.email}")
-        .get();
-
-    print(responseData.exists);
-
-    if (!responseData.exists) {
-      print("Log-Out Event");
-      final bool response = await GoogleAuth().logOut();
-
-      if (!response) FirebaseAuth.instance.signOut();
-      return IntroductionScreen();
-    }
-    return MainScreen();
-  } catch (e) {
-    print("Starting Error is: $e");
-    return IntroductionScreen();
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
